162187f2a1d6e7bfae77b8b2c2667921aaa659e2
Julien Couillard <julien.couillard@etudiant.univ-rennes1.fr>
2017-09-26 09:43:01 +0200

    radom is ready to work

--- index.js
+++ index.js
@@ -3,37 +3,14 @@ var app = express();
 
 const youtubeRouter = require('./router/youtube');
 
-app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
-
-app.get('/', function(req, res) {
-  res.send("Hello World!");
-});
-
-app.use('/youtube', youtubeRouter);
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
-
-var game = { "pos_player" : 1,  // ici 0, 1, 2, 3 --- 0 upper and 3 lowest 
-              "array_spectrum" : new Array(), // array of 0 and 1 --- 0 : small and 1 big
-              "array_artefacts": new Array() // array of 0, 1, 2, 3 --- 0 upper and 3 lowest 
-}; 
-
-
+// var array_spectrum = [0,0,0,1,1,0,1,0]; to test function below 
 
 function get_random(array_spectrum) 
 {
   //Example, including customisable intervals [lower_bound, upper_bound)
-  var lower_bound = 0;
-  var upper_bound = 0;
-  var unique_random_numbers = [];
-  var i = 0
-  while (unique_random_numbers.length <= array_spectrum.length) 
-  {
-    if (array_spectrum[i] == 0)
+  var random_numbers = [];
+  array_spectrum.forEach(function(element) {
+    if (element == 0)
     {
       var lower_bound = 1;
       var upper_bound = 2;
@@ -44,11 +21,28 @@ function get_random(array_spectrum)
       var upper_bound = 3;
     }
     var random_number = Math.round(Math.random()*(upper_bound - lower_bound) + lower_bound);
-    if (unique_random_numbers.indexOf(random_number) == -1) 
-    {
-        // Yay! new random number
-        unique_random_numbers.push( random_number );
-    }
-  }
-  return unique_random_numbers
-}
+    // Yay! new random number
+    random_numbers.push( random_number );
+  });
+  return random_numbers;
+};
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+  res.send("Yo !");
+});
+
+app.use('/youtube', youtubeRouter);
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+/*
+var game = { "pos_player" : 1,  // ici 0, 1, 2, 3 --- 0 upper and 3 lowest 
+              "array_spectrum" : new Array(), // array of 0 and 1 --- 0 : small and 1 big
+              "array_artefacts": new Array() // array of 0, 1, 2, 3 --- 0 upper and 3 lowest 
+}; 
+*/
