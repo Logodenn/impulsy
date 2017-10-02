c6b2316af846b0e8cba056861164a0fc4140eb9d
Julien Couillard <Texju@iMac-de-Julien.local>
2017-10-02 21:43:51 +0200

    delete get_random in double

--- modules/game_function.js
+++ modules/game_function.js
@@ -1,27 +1,5 @@
 var youtube = require('./youtube');
 
-function get_random(array_spectrum) 
-{
-  //Example, including customisable intervals [lower_bound, upper_bound)
-  var random_numbers = [];
-  array_spectrum.forEach(function(element) {
-    if (element == 0)
-    {
-      var lower_bound = 1;
-      var upper_bound = 2;
-    }
-    else 
-    {
-      var lower_bound = 0;
-      var upper_bound = 3;
-    }
-    var random_number = Math.round(Math.random()*(upper_bound - lower_bound) + lower_bound);
-    // Yay! new random number
-    random_numbers.push( random_number );
-  });
-  return random_numbers;
-};
-
 module.exports.checkRightPosition = function checkRightPosition(game, t) {
 	if ((game.position != game.artefact[t] & game.difficulty == "easy") | (game.position == game.artefact[t] & game.difficulty == "crazy")) game.energy = game.energy - 1
 	else if (game.position != game.artefact[t] & game.difficulty == "crazy") game.energy = game.energy - 2
@@ -29,7 +7,6 @@ module.exports.checkRightPosition = function checkRightPosition(game, t) {
 	else console.log("WTF !!!")
 	return game.energy;
 }
-
 
 
 // var array_spectrum = [0,0,0,1,1,0,1,0]; to test function below 
