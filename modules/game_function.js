589116eb3a8c5e187b930dccf20a659c6678ad0c
Julien Couillard <julien.couillard@etudiant.univ-rennes1.fr>
2017-10-02 15:41:49 +0200

    add random to game_function and use of gatAudioStream in createGame

--- modules/game_function.js
+++ modules/game_function.js
@@ -1,6 +1,26 @@
 var youtube = require('./youtube');
 
-
+function get_random(array_spectrum) 
+{
+  //Example, including customisable intervals [lower_bound, upper_bound)
+  var random_numbers = [];
+  array_spectrum.forEach(function(element) {
+    if (element == 0)
+    {
+      var lower_bound = 1;
+      var upper_bound = 2;
+    }
+    else 
+    {
+      var lower_bound = 0;
+      var upper_bound = 3;
+    }
+    var random_number = Math.round(Math.random()*(upper_bound - lower_bound) + lower_bound);
+    // Yay! new random number
+    random_numbers.push( random_number );
+  });
+  return random_numbers;
+};
 
 function checkRightPosition(game, t) {
 	if ((game.position != game.artefact[t] & game.difficulty == "easy") | (game.position == game.artefact[t] & game.difficulty == "crazy")) game.energy = game.energy - 1
@@ -39,13 +59,18 @@ function createGame(sound, difficulty, gameId, socketId, callback) {
 		position: 1, // here 0, 1, 2, 3 --- 0 upper and 3 lowest 
 		difficulty: difficulty // difficulty of the level 
 	};
-	youtube.getBars(sound, 1, function (erreur, bars) {
-		if (erreur) console.log(erreur);
+	youtube.getAudioStream(youtubeVideoId, function (err, stream) {
+		if (err) console.log(err);
 		else {
-			game.array_spectrum = bars;
-			game.array_artefacts = getRandom(game.array_spectrum); // array of 0, 1, 2, 3 --- 0 upper and 3 lowest 
-			game.energy = game.array_spectrum.length; // duration of the music 
-			callback(null, game)
+			youtube.getBars(stream, 1, function (err, bars) {
+				if (err) console.log(err);
+				else {
+					game.array_spectrum = bars;
+					game.array_artefacts = getRandom(game.array_spectrum); // array of 0, 1, 2, 3 --- 0 upper and 3 lowest 
+					game.energy = game.array_spectrum.length; // duration of the music 
+					callback(null, game)
+				}
+			});
 		}
 	});
 }
