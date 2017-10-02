062ca7299efbc0ff82d63df6ebfe74aca3f5a4a2
Julien Couillard <julien.couillard@etudiant.univ-rennes1.fr>
2017-10-02 15:32:52 +0200

    creation of game

--- modules/game_function.js
+++ modules/game_function.js
@@ -1,4 +1,4 @@
-
+var youtube = require('./youtube');
 
 
 
@@ -8,42 +8,44 @@ function checkRightPosition(game, t) {
 	else if (game.position == game.artefact[t] & game.difficulty == "easy") game.energy = game.energy
 	else console.log("WTF !!!")
 	return game.energy;
-  }
+}
 
 
 
-  // var array_spectrum = [0,0,0,1,1,0,1,0]; to test function below 
+// var array_spectrum = [0,0,0,1,1,0,1,0]; to test function below 
 
 function getRandom(array_spectrum) {
 	//Example, including customisable intervals [lower_bound, upper_bound)
 	var random_numbers = [];
 	array_spectrum.forEach(function (element) {
-	  if (element == 0) {
-		var lower_bound = 1;
-		var upper_bound = 2;
-	  } else {
-		var lower_bound = 0;
-		var upper_bound = 3;
-	  }
-	  var random_number = Math.round(Math.random() * (upper_bound - lower_bound) + lower_bound);
-	  // Yay! new random number
-	  random_numbers.push(random_number);
+		if (element == 0) {
+			var lower_bound = 1;
+			var upper_bound = 2;
+		} else {
+			var lower_bound = 0;
+			var upper_bound = 3;
+		}
+		var random_number = Math.round(Math.random() * (upper_bound - lower_bound) + lower_bound);
+		// Yay! new random number
+		random_numbers.push(random_number);
 	});
 	return random_numbers;
-  }
+}
 
-function createGame(sound, difficulty, gameId, socketId)
-{
-	// fonction pierre pour array_spectrum sound en param
-var game = {
-	gameId : gameId,
-	socketId : socketId,
-	position: 1, // here 0, 1, 2, 3 --- 0 upper and 3 lowest 
-	array_spectrum: array_spectrum, // array of 0 and 1 --- 0 : small and 1 big
-	array_artefacts: getRandom(array_spectrum), // array of 0, 1, 2, 3 --- 0 upper and 3 lowest 
-	energy: array_spectrum.length, // duration of the music 
-	difficulty : difficulty // difficulty of the level 
+function createGame(sound, difficulty, gameId, socketId, callback) {
+	var game = {
+		gameId: gameId,
+		socketId: socketId,
+		position: 1, // here 0, 1, 2, 3 --- 0 upper and 3 lowest 
+		difficulty: difficulty // difficulty of the level 
 	};
-
-	return game;
+	youtube.getBars(sound, 1, function (erreur, bars) {
+		if (erreur) console.log(erreur);
+		else {
+			game.array_spectrum = bars;
+			game.array_artefacts = getRandom(game.array_spectrum); // array of 0, 1, 2, 3 --- 0 upper and 3 lowest 
+			game.energy = game.array_spectrum.length; // duration of the music 
+			callback(null, game)
+		}
+	});
 }
