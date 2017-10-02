9fe9148b75934a46141df09f63ea532fdf0e8bbf
Benjent M <benjaminjeanmorvan@gmail.com>
2017-10-02 17:15:55 +0200

    Clean ws handlers

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,11 +1,117 @@
-// var model = {
-// 	gameName	: "Impulsy",
-// 	catchPhrase	: "Ride the music!"
-// }
 
-/*var context = {gameName: "Impulsy", catchPhrase: "Ride the music!"};
-var html    = template(context);*/
+// ******************************************************************* //
+// ******************** WEBSOCKET INITIALIZATION ******************** //
+// ***************************************************************** //
 
+// ******************** IO ******************** //
+
+var IO = {
+
+    init: function() {
+        IO.socket = io.connect();
+        IO.bindEvents();
+    },
+
+    bindEvents : function() {
+        IO.socket.on('connected', IO.onConnected);
+        IO.socket.on('newGameCreated', IO.onNewGameCreated);
+        IO.socket.on('gameStarted', IO.onGameStarted);
+        IO.socket.on('playerMove', IO.playerMove);
+        IO.socket.on('gameOver', IO.gameOver);
+        // IO.socket.on('error', IO.error );
+    },
+
+    onConnected : function() {
+        // Cache a copy of the client's socket.IO session ID on the App
+        App.mySocketId = IO.socket.sessionid;
+        // console.log(data.message);
+    },
+
+    onNewGameCreated : function(data) {
+		// gameId
+		// mySocketId
+        App.Host.gameInit(data);
+    },
+
+    onGameStarted : function(data) {
+		startGameTmp();
+		// TODO
+    },
+
+    playerMove : function(data) {
+		// TODO
+		// Notify players that a player has moved
+	},
+
+	gameOver : function(data) {
+		endGame();
+		// TODO
+		// Notify players that game has ended
+		// remove listeners
+    }
+};
+
+// ******************** App ******************** //
+
+var App = {
+
+    gameId: 0,
+    myRole: '',   // 'Player' or 'Host'
+
+    /**
+     * The Socket.IO socket object identifier. This is unique for
+     * each player and host. It is generated when the browser initially
+     * connects to the server when the page loads for the first time.
+     */
+    mySocketId: '',
+
+    init: function () {
+        App.bindEvents();
+    },
+
+    // Event handlers for buttons
+    bindEvents: function () {
+		// Host
+		document.querySelector("#startGameButton").on('click', App.Host.onCreateClick);
+
+        // Player
+		// App.$doc.on('click', '.btnAnswer',App.Player.onPlayerAnswerClick);
+		// TODO gérer playerMove ici ?
+    },
+
+	// ********** Host ********** //
+    Host : {
+
+        players : [],
+        currentCorrectAnswer: '',
+
+        onCreateClick: function () {
+            // console.log('Clicked "Create A Game"');
+            IO.socket.emit('hostCreateNewGame');
+        },
+
+        gameInit: function (data) {
+            App.gameId 		= data.gameId;
+            App.mySocketId 	= data.mySocketId;
+            App.myRole 		= 'Host';
+            // console.log("Game started with ID: " + App.gameId + ' by host: ' + App.mySocketId);
+        }
+	},
+	
+	// ********** Player ********** //
+    Player : {
+		onMove : function(data) {
+			IO.socket.emit('hostCreateNewGame');
+		}
+	}
+};
+
+IO.init();
+App.init();
+
+// ******************************************************* //
+// ******************** CANVAS SETUP ******************** //
+// ***************************************************** //
 
 // ******************** Canvas units ******************** //
 
@@ -25,168 +131,84 @@ var COLOR = {
 }
 
 // ***************************************************** //
-// ******************** START GAME ******************** //
+// ******************** GAME LOGIC ******************** //
 // *************************************************** //
 
-// ******************** Websocket handlers ******************** //
+function startGameTmp() {
+
+	// ******************** Player movement ******************** //
+	
+	window.onkeyup = function(e) {
+		
+		// TODO: bind with canvas drawing
+		// fill(COLOR.player);
+		// rect(left, top, blocUnit, height);
+	
+		var key = e.keyCode ? e.keyCode : e.which;
+		// a : top = 65
+		// z : midtop = 90
+		// e : midbot = 69
+		// r : bot = 82
+		// Up: 38
+		// Down: 40
+		switch (key) {
+			case 65:
+				// Top
+				playerPosition = 0;
+				player.y = 163;
+				break;
+			case 90:
+				// Midtop
+				playerPosition = 1;
+				break;
+			case 69:
+				// Midbot
+				playerPosition = 2;
+				break;
+			case 82:
+				// Bot
+				playerPosition = 3;
+				break;
+			case 38:
+				// Up arrow
+				if(playerPosition != 0) {
+	
+					playerPosition--;
+				}
+				break;
+			case 40:
+				// Down arrow
+				if(playerPosition != 3) {
+	
+					playerPosition++;
+				}
+				break;
+		}
+	
+		// ******************** Notify websocket ******************** //
+		console.log("trying to emit new position through ws");
+		gameSocket.emit('playerMove', {message: "The player position is now:" + playerPosition});
+	}
+}
 
 
-var IO = {
-
-    init: function() {
-        IO.socket = io.connect();
-        IO.bindEvents();
-    },
-
-    bindEvents : function() {
-        IO.socket.on('connected', IO.onConnected );
-        IO.socket.on('newGameCreated', IO.onNewGameCreated );
-        // IO.socket.on('beginNewGame', IO.beginNewGame );
-        IO.socket.on('playerMove', IO.onNewWordData);
-        // IO.socket.on('hostCheckAnswer', IO.hostCheckAnswer);
-        // IO.socket.on('gameOver', IO.gameOver);
-        // IO.socket.on('error', IO.error );
-    },
-
-    onConnected : function() {
-        // Cache a copy of the client's socket.IO session ID on the App
-        App.mySocketId = IO.socket.sessionid;
-        // console.log(data.message);
-    },
-
-    /**
-     * A new game has been created and a random game ID has been generated.
-     * @param data {{ gameId: int, mySocketId: * }}
-     */
-    onNewGameCreated : function(data) {
-        App.Host.gameInit(data);
-    },
-
-    /**
-     * Both players have joined the game.
-     * @param data
-     */
-    beginNewGame : function(data) {
-        // App[App.myRole].gameCountdown(data);
-    },
-
-    playerMove : function(data) {
-        // Update the current round
-        // App.currentRound = data.round;
-
-        // Change the word for the Host and Player
-        // App[App.myRole].newWord(data);
-    }
-};
-
-var App = {
-
-    gameId: 0,
-    myRole: '',   // 'Player' or 'Host'
-
-    /**
-     * The Socket.IO socket object identifier. This is unique for
-     * each player and host. It is generated when the browser initially
-     * connects to the server when the page loads for the first time.
-     */
-    mySocketId: '',
-
-    /* *************************************
-     *                Setup                *
-     * *********************************** */
-
-    init: function () {
-        // App.cacheElements();
-        App.bindEvents();
-
-        // Initialize the fastclick library
-        // FastClick.attach(document.body);
-    },
-
-    /**
-     * Create some click handlers for the various buttons that appear on-screen.
-     */
-    bindEvents: function () {
-        // Host
-        //App.$doc.on('click', '#btnCreateGame', App.Host.onCreateClick);
-
-        // Player
-        // App.$doc.on('click', '#btnJoinGame', App.Player.onJoinClick);
-        // App.$doc.on('click', '#btnStart',App.Player.onPlayerStartClick);
-        // App.$doc.on('click', '.btnAnswer',App.Player.onPlayerAnswerClick);
-    },
-
-    /* *******************************
-       *         HOST CODE           *
-       ******************************* */
-    Host : {
-
-        players : [],
-        currentCorrectAnswer: '',
-
-        /**
-         * Handler for the "Start" button on the Title Screen.
-         */
-        onCreateClick: function () {
-            // console.log('Clicked "Create A Game"');
-            IO.socket.emit('hostCreateNewGame');
-        },
-
-        gameInit: function (data) {
-            App.gameId = data.gameId;
-            App.mySocketId = data.mySocketId;
-            App.myRole = 'Host';
-            // App.Host.numPlayersInRoom = 0;
-
-            // App.Host.displayNewGameScreen();
-            // console.log("Game started with ID: " + App.gameId + ' by host: ' + App.mySocketId);
-        },
-
-        playerMove : function(data) {
-
-            // Update the data for the current round
-            //App.Host.currentCorrectAnswer = data.answer;
-            //App.Host.currentRound = data.round;
-        }
-    }
-
-};
-
-IO.init();
-App.init();
-
-
-
-
-
-
-
-
-
-
-
-// var io;
-// var gameSocket;
-// var game;
-
-// conncetion
-
-// // exports.init_game = function (sio, socket) {
-// function wsGenerator(sio, socket) {
-//   io = sio;
-//   gameSocket = socket;
-//   gameSocket.emit('connected', {message: "You are connected!"});
-
-//   console.log("ouverture webs");
-
-//   // Host Events
-//   gameSocket.on('hostCreateNewGame', hostCreateNewGame);
-
-//   // Player Events
-//   gameSocket.on('playerMove', playerMove);
+// var model = {
+// 	gameName	: "Impulsy",
+// 	catchPhrase	: "Ride the music!"
 // }
 
-// wsGenerator();
+/*var context = {gameName: "Impulsy", catchPhrase: "Ride the music!"};
+var html    = template(context);*/
+
+
+
+
+
+
+
+
+
+
 
 var amplitudes = [0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0];
 var artefacts =  [1, 0, 2, 3, 0, 1, 2, 2, 0, 1, 2, 1, 1, 2, 2, 3, 0, 1, 2, 1, 1, 0, 2, 2, 1, 1, 1, 1, 2, 3, 0, 1, 2, 1, 0, 1, 2, 1, 1, 1, 2, 3, 0, 1, 2, 1, 2, 1, 2, 1, 2, 1];
@@ -320,54 +342,4 @@ function addAmplitudeAndArtefact() {
 
 
 
-window.onkeyup = function(e) {
 
-	// TODO: bind with cancas drawing
-	// fill(COLOR.player);
-	// rect(left, top, blocUnit, height);
-
-	var key = e.keyCode ? e.keyCode : e.which;
-	// a : top = 65
-	// z : midtop = 90
-	// e : midbot = 69
-	// r : bot = 82
-	// Up: 38
-	// Down: 40
-	switch (key) {
-		case 65:
-			// Top
-			playerPosition = 0;
-			player.y = 163;
-			break;
-		case 90:
-			// Midtop
-			playerPosition = 1;
-			break;
-		case 69:
-			// Midbot
-			playerPosition = 2;
-			break;
-		case 82:
-			// Bot
-			playerPosition = 3;
-			break;
-		case 38:
-			// Up arrow
-			if(playerPosition != 0) {
-
-				playerPosition--;
-			}
-			break;
-		case 40:
-			// Down arrow
-			if(playerPosition != 3) {
-
-				playerPosition++;
-			}
-			break;
-	}
-
-    // ******************** Notify websocket ******************** //
-    console.log("trying to emit new position through ws");
-    gameSocket.emit('playerMove', {message: "The player position is now:" + playerPosition});
-}
