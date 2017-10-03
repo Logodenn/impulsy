ca96ba28d3c187970a5297ee21b4989ac1205e72
Thomas Kermarrec <thomas@dy083198.etu.univ-rennes1.fr>
2017-10-03 11:20:22 +0200

    add new canvas code

--- assets/js/game.js
+++ assets/js/game.js
@@ -144,9 +144,31 @@ var COLOR = {
 	player 		: [200, 10, 100] //
 }
 
+var r = 50;
+var g = 200;
+var b = 255;
+var colorToChange = "g-";
+var counterForAmplitudeColor = 0;
+var counterForColorTab = 0;
+var tabColorToChange = ["g-", "r+", "r-", "g+"];
+
 // ***************************************************** //
 // ******************** GAME LOGIC ******************** //
 // *************************************************** //
+
+// ******************** Game variables ******************** //
+
+var amplitudes = [0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0];
+var artefacts =  [1, 0, 2, 3, 0, 1, 2, 2, 0, 1, 2, 1, 1, 2, 2, 3, 0, 1, 2, 1, 1, 0, 2, 2, 1, 1, 1, 1, 2, 3, 0, 1, 2, 1, 0, 1, 2, 1, 1, 1, 2, 3, 0, 1, 2, 1, 2, 1, 2, 1, 2, 1];
+var time = 0;
+
+// ******************** Components ******************** //
+
+var player;
+var energyBar;
+var pulsers;
+var listeBarres = [];
+var listeArtefacts = [];
 
 function startGameTmp() {
 
@@ -214,3 +236,251 @@ function startGameTmp() {
 
 /*var context = {gameName: "Impulsy", catchPhrase: "Ride the music!"};
 var html    = template(context);*/
+
+
+
+
+// ***************************************************** //
+// ******************** GAME LOGIC ******************** //
+// *************************************************** //
+
+
+function startGame() {
+	myGameArea.start();
+	player = new Player()
+	energyBar = new EnergyBar()
+	pulsers   = new Pulsers()
+}
+
+
+var myGameArea = {
+	canvas : document.createElement("canvas"),
+	start : function() {
+		this.canvas.width = 1000;
+		this.canvas.height = canvasHeight;
+		this.context = this.canvas.getContext("2d");
+		document.body.insertBefore(this.canvas, document.querySelector("#canvasWrapper"));
+		this.intervalAddAmplitude = setInterval("addAmplitudeAndArtefact();",500);
+		this.intervalUpdate = setInterval("updateGameArea();", 10);
+	},
+	clear : function() {
+		this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+	},
+	stop : function() {
+		clearInterval(this.intervalUpdate);
+	},
+	stopAddition : function() {
+		clearInterval(this.intervalAddAmplitude);
+	}
+}
+
+
+function Player() {
+	var self = this;
+	self.x = 400;
+	self.y = smallBarTop;
+	self.img = new Image();
+	self.img.src = "licorne.png";
+	self.update = function() {
+		ctx = myGameArea.context;
+		ctx.drawImage(self.img, self.x, self.y, blocUnit, blocUnit);
+	}
+
+	self.ctx = myGameArea.context;
+	self.ctx.drawImage(self.img, self.x, self.y, blocUnit, blocUnit);
+}
+
+function Pulsers() {
+	var self = this;
+	self.x = 890;
+	self.y = 150;
+	self.img = new Image();
+	self.img.src = "pulsers.png";
+	self.update = function() {
+		ctx = myGameArea.context;
+		ctx.drawImage(self.img, self.x, self.y, 103, 406);
+	}
+
+	self.ctx = myGameArea.context;
+	self.ctx.drawImage(self.img, self.x, self.y, 103, 406);
+}
+
+function Artefact(posY) {
+	var self = this;
+	self.x = myGameArea.canvas.width - 145;
+	self.y = posY;
+	self.img = new Image();
+	self.img.src = "artefact.png";
+	self.update = function() {
+		this.x -= 1;
+		ctx = myGameArea.context;
+		ctx.drawImage(self.img, self.x, self.y, 20, 34);
+	}
+
+	self.ctx = myGameArea.context;
+	ctx.drawImage(self.img, self.x, self.y, blocUnit, blocUnit);
+}
+
+function Amplitude(height) {
+	switch (tabColorToChange[counterForColorTab]) {
+		case "r+":
+		r += 15;
+		break;
+		case "r-":
+		r -= 15;
+		break;
+		case "g+":
+		g += 15;
+		break;
+		case "g-":
+		g -= 15;
+		break;
+		case "b+":
+		b += 15;
+		break;
+		case "b-":
+		b -= 15;
+		break;
+		default:
+	}
+
+	if (counterForAmplitudeColor == 9) {
+		counterForAmplitudeColor = 0;
+
+		if (counterForColorTab == 3) {
+			counterForColorTab = 0;
+		} else {
+			counterForColorTab++;
+		}
+	} else {
+		counterForAmplitudeColor++;
+	}
+
+	this.color = "rgb(" + r.toString() + "," + g.toString() + "," + b.toString() + ")";
+	console.log(this.color);
+	this.width = 25;
+	this.height = height ? bigBarHeight : smallBarHeight;
+	this.x = myGameArea.canvas.width - 130;
+	this.y = canvasHeight / 2 - this.height / 2;
+	this.update = function() {
+		this.x -= 1;
+		ctx = myGameArea.context;
+		ctx.fillStyle = this.color;
+		ctx.fillRect(this.x, this.y, this.width, this.height);
+	}
+
+	this.ctx = myGameArea.context;
+	this.ctx.fillStyle = this.color;
+	this.ctx.fillRect(this.x, this.y, this.width, this.height);
+}
+
+function EnergyBar() {
+	this.color = "#FFD51D";
+	this.width = 500;
+	this.height = 25;
+	this.x = 250;
+	this.y = 25;
+	this.update = function() {
+		ctx = myGameArea.context;
+		ctx.fillStyle = this.color;
+		ctx.fillRect(this.x, this.y, this.width, this.height);
+	}
+
+	this.ctx = myGameArea.context;
+	this.ctx.fillStyle = this.color;
+	this.ctx.fillRect(this.x, this.y, this.width, this.height);
+}
+
+
+function updateGameArea() {
+	myGameArea.clear();
+
+	for (i = 0; i < listeBarres.length; i += 1) {
+		listeBarres[i].update();
+		listeArtefacts[i].update();
+	}
+
+	pulsers.update();
+	energyBar.update();
+	player.update();
+}
+
+
+function addAmplitudeAndArtefact() {
+	var amplitude  = new Amplitude(amplitudes[time]);
+	listeBarres.push(amplitude);
+
+	var artefact;
+	switch (artefacts[time]) {
+		case 0:
+		artefact  = new Artefact(blocUnit);
+		break;
+		case 1:
+		artefact  = new Artefact(2 * blocUnit);
+		break;
+		case 2:
+		artefact  = new Artefact(3 * blocUnit);
+		break;
+		case 3:
+		artefact  = new Artefact(4 * blocUnit);
+		break;
+	}
+	listeArtefacts.push(artefact);
+
+	time++;
+
+	if(time > artefacts.length) {
+		myGameArea.stopAddition();
+	}
+}
+
+
+
+window.onkeydown = function(e) {
+	var key = e.keyCode ? e.keyCode : e.which;
+	// a : top = 65
+	// z : midtop = 90
+	// e : midbot = 69
+	// r : bot = 82
+	// Up: 38
+	// Down: 40
+	switch (key) {
+		case 65:
+		// Top
+		playerPosition = 0;
+		player.y = 163;
+		break;
+		case 90:
+		// Midtop
+		playerPosition = 1;
+		player.y = 263;
+		break;
+		case 69:
+		// Midbot
+		playerPosition = 2;
+		player.y= 363;
+		break;
+		case 82:
+		// Bot
+		playerPosition = 3;
+		player.y= 463;
+		break;
+		case 38:
+		// Up arrow
+		if(playerPosition != 0) {
+
+			playerPosition--;
+			player.y -= 100;
+		}
+		break;
+		case 40:
+		// Down arrow
+		if(playerPosition != 3) {
+
+			playerPosition++;
+			player.y += 100;
+		}
+		break;
+	}
+	player.update();
+}
