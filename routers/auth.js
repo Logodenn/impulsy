259c581651784f37b7de82c823f458eda7044c63
Julien Couillard <Texju@iMac-de-Julien.local>
2017-10-15 21:24:48 +0200

    auth with passport

--- routers/auth.js
+++ routers/auth.js
@@ -1,104 +1,102 @@
 /*
 
-var path = require('path');
-var favicon = require('static-favicon');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
- 
-// These are the new imports we're adding:
-var passport = require('passport');
-var StormpathStrategy = require('passport-stormpath');
-var session = require('express-session');
-var flash = require('connect-flash'); // peut être inutile 
+TODO : dans le index.js
 
-// j'ai un doute sur l'utilisation de passport 
-// mais ça pertmet d'utiliser FB, Google etc... 
-
-
-// Here is what we're adding:
-var strategy = new StormpathStrategy();
-passport.use(strategy);
-passport.serializeUser(strategy.serializeUser);
-passport.deserializeUser(strategy.deserializeUser);
-
-
-app.use(favicon()); // je ne sais pas ce que ça fait
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(cookieParser());
-app.use(require('stylus').middleware(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public')));
- 
-// Stuff we're adding:
 app.use(session({
-  secret: process.env.EXPRESS_SECRET,
-  key: 'sid',
-  cookie: { secure: false },
+  cookieName: 'session',
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
 }));
+
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(flash());
 
+
+TODO : dans le user_controller.js ou user.js
+var passport = require('passport');
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var passportLocalMongoose = require('passport-local-mongoose');
+
+var Account = new Schema({
+    username: String,
+    password: String
+});
+
+Account.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('Account', Account);
 
 */
-// Render the registration page.
-router.get('/register', function(req, res) {
-  res.render('register', { title: 'Register', error: req.flash('error')[0] });
+
+var express = require('express');
+var passport = require('passport'); 
+var Account = require('../models/controllers/user_controllers.js');
+
+var router = express.Router();
+
+router.get('/', function (req, res) {
+  res.render('index', { user : req.user });
 });
- 
-// Register a new user to Stormpath.
-router.post('/register', function(req, res) {
-  var username = req.body.username;
-  var password = req.body.password;
-  var email = req.body.email;
- 
-  // Grab user fields.
-  if (!username || !password) {
-    return res.render('register', { title: 'Register', error: 'Email and password required.' });
+
+router.get('/register', function(req, res) {
+  if ( req.session.passport.user != null ) {
+    res.redirect('/');
+  } else {
+    res.render('register', {
+      title : 'Sign-up'
+    });
   }
- 
-  // Initialize our Stormpath client.
-  var apiKey = new stormpath.ApiKey(
-    process.env['STORMPATH_API_KEY_ID'],
-    process.env['STORMPATH_API_KEY_SECRET']
-  );
-  var spClient = new stormpath.Client({ apiKey: apiKey });
- 
-  // Grab our app, then attempt to create this user's account.
-  spClient.getApplication(process.env['STORMPATH_APP_HREF'], function(err, app) {
-    if (err) throw err;
- 
-    app.createAccount({
-      username: username,
-      email: email,
-      password: password
-    }, function (err, createdAccount) {
-      if (err) {
-        return res.render('register', {title: 'Register', error: err.userMessage});
-      }
- 
-      passport.authenticate('stormpath')(req, res, function () {
-        return res.redirect('/game');
+});
+
+router.post('/register', function(req, res, next) {
+  Account.register(new Account({ username : req.body.username }), req.body.password, function(err, account) {
+    if (err) {
+      return res.render('register', { error : err.message });
+    }
+
+    passport.authenticate('local')(req, res, function () {
+      req.session.save(function (err) {
+        if (err) {
+          return next(err);
+        }
+        res.redirect('/');
       });
     });
   });
 });
-// Render the login page.
+
 router.get('/login', function(req, res) {
-  res.render('login', { title: 'Login', error: req.flash('error')[0] });
+  if ( req.session.passport.user != null ) {
+    res.redirect('/');
+  } else {
+    res.render('login', {
+      user : req.user,
+      title : 'Sign-in',
+      subTitle : 'Come back please !'
+    });
+  }
 });
- 
-// Authenticate a user.
-router.post('/login', passport.authenticate('stormpath', {
-  successRedirect: '/game',
-  failureRedirect: '/login',
-  failureFlash: 'Invalid email or password.'
-}));
- 
-// Logout the user, then redirect to the home page.
+
+router.post('/login', passport.authenticate('local'), function(req, res) {
+  if ( req.session.passport.user != null ) {
+    res.redirect('/');
+  } else {
+    res.redirect('/register');
+  }
+});
+
 router.get('/logout', function(req, res) {
-  req.logout();
-  res.redirect('/');
+  if ( req.session.passport.user != null ) {
+    req.logout();
+    res.redirect('/');
+  }
+  else {
+    res.redirect('/')
+  }
 });
- 
+
+
 module.exports = router;
