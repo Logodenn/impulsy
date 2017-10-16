77c7f32c4c56c61b49d11ed6fc1c8a6223302d95
berrou <glennberr29@gmail.com>
2017-10-16 17:25:45 +0200

    delete update read

--- routers/score.js
+++ routers/score.js
@@ -3,12 +3,17 @@ const express = require('express');
 const router = express.Router();
 var moment = require('moment');
 var helpers = require('./_helpers');
+const logger = require('winston');
+
 
 
 router.get("/", (req, res, next)=> {
 
     req.models.score.find().limit(4).all(function (err, scores) {
-        if (err) return next(err);
+        if (err) {
+            logger.debug(err);
+            return next(err);
+        }
 
         var items = scores.map(function (m) {
             return m.serialize();
@@ -24,12 +29,13 @@ router.post("/", (req, res, next) => {
 
     var params = req.body;
 
-    console.log(params.user_id);
     req.models.user.get(params.user_id, function (err, user) {
         if (err) {
             if (err.code == orm.ErrorCodes.NOT_FOUND) {
+                logger.debug(err);
                 return res.status(404).send("User not found");
             } else {
+                logger.debug(err);
                 return next(err);
             }
         }
@@ -37,28 +43,92 @@ router.post("/", (req, res, next) => {
         req.models.track.get(params.track_id, function (err, track) {
             if (err) {
                 if (err.code == orm.ErrorCodes.NOT_FOUND) {
+                    logger.debug(err);
                     return res.status(404).send("Track not found");
                 } else {
+                    logger.debug(err);
                     return next(err);
                 }
             }
 
-            params.user_date = user.pseudo;
-            params.track_date = track.name;
+            // params.user_date = user.pseudo;
+            // params.track_date = track.name;
             params.date = new Date().toLocaleString();
-            console.log(params.date);
-            console.log(params);
+
             req.models.score.create(params, function (err, score) {
                 if (err) {
                     if (Array.isArray(err)) {
+                        logger.debug(err);
                         return res.status(200).send({errors: helpers.formatErrors(err)});
                     } else {
+                        logger.debug(err)
                         return next(err);
                     }
                 }
 
+                logger.info("score "+score.id+" created !");
+
                 return res.status(200).send(score.serialize());
             });
+        });
+    });
+});
+
+router.delete("/", (req, res, next) => {
+    if (!req.body) return res.sendStatus(400);
+
+    var params = req.body;
+
+    req.models.score.get(params.score_id, function (err, score) {
+        if (err) {
+            if (err.code == orm.ErrorCodes.NOT_FOUND) {
+                logger.debug(err);
+                return res.status(404).send("Score not found");
+            } else {
+                logger.debug(err);
+                return next(err);
+            }
+        }
+        score.remove(function (err) {
+            if (err) {
+                logger.debug(err);
+                return next(err);
+            }
+
+            logger.info("score " + score.id + " removed !");
+        });
+
+        return res.status(202).send('removed');
+    });
+});
+
+router.post("/update", (req, res, next) => {
+
+    if (!req.body) return res.sendStatus(400);
+
+    var params = req.body;
+
+    req.models.score.get(params.score_id, function (err, score) {
+        if (err) {
+            if (err.code == orm.ErrorCodes.NOT_FOUND) {
+                logger.debug(err);
+                return res.status(404).send("Score not found");
+            } else {
+                logger.debug(err);
+                return next(err);
+            }
+        }
+        if (params.date) score.date = params.date;
+
+        if (params.duration) score.duration = params.duration;
+
+
+        score.save(function (err) {
+            if (err) {
+                logger.debug(err);
+                return next(err);
+            }
+            return res.status(200).send(score.serialize());
         });
     });
 });
