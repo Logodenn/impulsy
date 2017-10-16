50e5ef0013cab21c565097d9bf38c5a2e5c2aa90
berrou <glennberr29@gmail.com>
2017-10-16 17:25:45 +0200

    delete update read

--- routers/track.js
+++ routers/track.js
@@ -2,11 +2,16 @@ var orm = require('orm');
 const express = require('express');
 const router = express.Router();
 var helpers = require('./_helpers');
+const logger = require('winston');
+
 
 
 router.get("/", (req, res, next)=> {
     req.models.track.find().limit(4).all(function (err, tracks) {
-        if (err) return next(err);
+        if (err) {
+            logger.debug(err);
+            return next(err);
+        }
 
         var items = tracks.map(function (m) {
             return m.serialize();
@@ -16,13 +21,26 @@ router.get("/", (req, res, next)=> {
     });
 });
 
+router.get("/:trackId", (req, res, next) => {
+    req.models.track.get(req.params.trackId, function (err, track) {
+        //track.getScores();
+        req.models.score.find({track_id: track.id}, function (err, scores) {
+            if (err){
+                logger.debug(err);
+            }
+            track.scores = scores;
+            var item = track.serialize();
+            res.send({item: item});
+        });
+    });
+});
 
 router.post("/", (req, res, next)=> {
     if (!req.body) return res.sendStatus(400);
 
     var params = req.body;
     params.information=JSON.stringify(params.information);
-    console.log(params.information)
+
     req.models.track.create(params, function (err, track) {
         if(err) {
             if(Array.isArray(err)) {
@@ -31,9 +49,82 @@ router.post("/", (req, res, next)=> {
                 return next(err);
             }
         }
+
+        logger.info("track"+track.id+" created !");
+
         return res.status(200).send(track.serialize())
     });
 });
+
+router.delete("/", (req, res, next) => {
+    if (!req.body) return res.sendStatus(400);
+
+    var params = req.body;
+
+    req.models.track.get(params.track_id, function (err, track) {
+        if (err) {
+            if (err.code == orm.ErrorCodes.NOT_FOUND) {
+                return res.status(404).send("Track not found");
+            } else {
+                logger.debug(err);
+                return next(err);
+            }
+        }
+        track.remove(function (err) {
+            if (err) {
+                logger.debug(err);
+                return next(err);
+            }
+
+            logger.info("track " + track.id + " removed !");
+        });
+
+        req.models.score.find({track_id: track.id}).remove(function (err) {
+            if (err) {
+                logger.debug(err);
+                return next(err);
+            }
+
+            logger.info("scores for track " + track.id + " removed !");
+        });
+
+        return res.status(202).send('removed');
+    });
+});
+
+
+router.post("/update", (req, res, next) => {
+
+    if (!req.body) return res.sendStatus(400);
+
+    var params = req.body;
+
+    req.models.track.get(params.track_id, function (err, track) {
+        if (err) {
+            if (err.code == orm.ErrorCodes.NOT_FOUND) {
+                return res.status(404).send("Track not found");
+            } else {
+                return next(err);
+            }
+        }
+        if (params.name) track.name = params.name;
+
+        if (params.link) track.link = params.link;
+
+        if (params.information) track.information = params.information;
+
+        logger.info("track"+track.id+" updated !");
+
+        track.save(function (err) {
+            if (err) {
+                logger.debug(err);
+                return next(err);
+            }
+            return res.status(202).send(track.serialize());
+        });
+    });
+});
+
 
 module.exports = router;
 
--- routers/user.js
+++ routers/user.js
@@ -2,35 +2,134 @@ var orm = require('orm');
 const express = require('express');
 const router = express.Router();
 var helpers = require('./_helpers');
+const logger = require('winston');
 
+logger.level = 'debug';
 
-router.get("/", (req, res, next)=> {
+router.get("/", (req, res, next) => {
     req.models.user.find().limit(4).all(function (err, users) {
-        if (err) return next(err);
+        if (err) {
+            logger.debug(err);
+            return next(err);
+        }
 
-        var items = users.map(function (m) {
-            return m.serialize();
+        var items = users.map(function (u) {
+            return u.serialize();
         });
-
-        res.send({ items: items });
+        res.send({items: items});
     });
 });
 
-router.post("/", (req, res, next)=> {
+router.get("/:userId", (req, res, next) => {
+    req.models.user.get(req.params.userId, function (err, user) {
+        //user.getScores();
+         //   if (err) return next(err);});
+        //user.getUsers();
+        if (user) {
+            req.models.score.find({user_id: user.id}, function (err, scores) {
+                if (err) {
+                    logger.debug(err);
+                }
+                user.scores = scores;
+                var item = user.serialize();
+                res.send({item: item});
+            });
+        } else {
+            logger.debug("user "+req.params.userId+" undefined");
+            return res.status(400).send("undefined");
+        }
+    });
+});
+
+
+router.post("/", (req, res, next) => {
 
     if (!req.body) return res.sendStatus(400);
 
     var params = req.body;
 
     req.models.user.create(params, function (err, user) {
-        if(err) {
-            if(Array.isArray(err)) {
-                return res.send(200, { errors: helpers.formatErrors(err) });
+        if (err) {
+            if (Array.isArray(err)) {
+                logger.debug(err);
+                return res.send(200, {errors: helpers.formatErrors(err)});
             } else {
+                logger.debug(err);
                 return next(err);
             }
         }
+        logger.info("user " + user.id + " created !");
         return res.status(200).send(user.serialize())
+    });
+});
+
+router.delete("/", (req, res, next) => {
+    if (!req.body) return res.sendStatus(400);
+
+    var params = req.body;
+
+    req.models.user.get(params.user_id, function (err, user) {
+        if (err) {
+            if (err.code == orm.ErrorCodes.NOT_FOUND) {
+                logger.debug(err);
+                return res.status(404).send("User not found");
+            } else {
+                logger.debug(err);
+                return next(err);
+            }
+        }
+        user.remove(function (err) {
+            if (err) {
+                logger.debug(err);
+                return next(err);
+            }
+
+            logger.info("user " + user.id + " removed !");
+        });
+
+        req.models.score.find({user_id: user.id}).remove(function (err) {
+            if (err) return next(err);
+
+            logger.info("scores for user " + user.id + " removed !");
+        });
+
+        return res.status(202).send('removed');
+    });
+});
+
+router.post("/update", (req, res, next) => {
+
+    if (!req.body) return res.sendStatus(400);
+
+    var params = req.body;
+
+    req.models.user.get(params.user_id, function (err, user) {
+        if (err) {
+            if (err.code == orm.ErrorCodes.NOT_FOUND) {
+                logger.debug(err);
+                return res.status(404).send("User not found");
+            } else {
+                logger.debug(err);
+                return next(err);
+            }
+        }
+        if (params.pseudo) user.pseudo = params.pseudo;
+
+        if (params.password) user.password = params.password;
+
+        if (params.mail) user.mail = params.mail;
+
+        if (params.rank) user.rank = params.rank;
+
+        logger.info("user"+user.id+" updated !");
+
+        user.save(function (err) {
+            if (err) {
+                logger.debug(err);
+                return next(err);
+            }
+            return res.status(202).send(user.serialize());
+        });
     });
 });
 
