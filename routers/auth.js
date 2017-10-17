b87e82d7d24f8398faf02d492640ac7102616e49
Julien Couillard <julien.couillard@etudiant.univ-rennes1.fr>
2017-10-17 14:40:22 +0200

    vérif avec Pierre pour auth

--- routers/auth.js
+++ routers/auth.js
@@ -1,51 +1,23 @@
-/*
-
-TODO : dans le index.js
-
-app.use(session({
-  cookieName: 'session',
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: false
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-*/
-
 var express = require('express');
 var passport = require('passport'); 
 var Account = require('../models/controllers/user_controller');
 
 var router = express.Router();
 
-router.get('/', function (req, res) {
-  res.render('index', { user : req.user });
-});
-
+//TODO : vérifier si l'user n'est pas déjà connecté 
 router.get('/register', function(req, res) {
-  if ( req.session.passport.user != null ) {
-    res.redirect('/');
-  } else {
-    res.render('register', {
-      title : 'Sign-up'
-    });
-  }
+    res.render('register');
 });
 
+//TODO : à l'aide Pierre 
+// Faire un create sur User
 router.post('/register', function(req, res, next) {
-  Account.register(new Account({ username : req.body.username }), req.body.password, function(err, account) {
+  User.register(new Account({ username : req.body.username }), req.body.password, function(err, account) {
     if (err) {
       return res.render('register', { error : err.message });
     }
-    passport.authenticate('local')(req, res, function () {
-      req.session.save(function (err) {
-        if (err) {
-          return next(err);
-        }
-        res.redirect('/');
-      });
+    passport.authenticate('local', function(req, res) {
+      res.redirect('/');
     });
   });
 });
