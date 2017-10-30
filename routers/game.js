c2e5ac632ff40eb7d60935cbec2a3bd97158de2d
Julien Couillard <julien.couillard@etudiant.univ-rennes1.fr>
2017-10-30 15:51:06 +0100

    track selection most played track

--- routers/game.js
+++ routers/game.js
@@ -1,15 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const db = require('./models/controllers');
+
 
 router
 .get("/", (req, res) => {
-    //{tickets:tickets}
-    // Trend track --> Youtube ? 
-    // Most played track 
-    // Favorite track --> player connected ? 
-    // 
-    res.render('game', { mostPlayed: db.models.track.mostPlayed });
-});
+            res.render('game', { message :"hello world"  });
+        });
 
 module.exports = router;
