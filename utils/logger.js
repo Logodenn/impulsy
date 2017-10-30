08c1cab244722ab8e3c1a1721bfdcf34e5b15ce0
Qayou <charpentier.pierre@outlook.com>
2017-10-30 10:55:41 +0100

    Update logger and remove unused file

--- utils/logger.js
+++ utils/logger.js
@@ -1,20 +1,19 @@
-const winston = require('winston');
-var path = require('path');
+const winston = require('winston')
 
-const baseDir = __dirname.slice(0, __dirname.length - "utils".length);
-const tsFormat = () => (new Date()).toLocaleTimeString();
+const baseDir = __dirname.slice(0, __dirname.length - '/utils'.length)
+const tsFormat = () => (new Date()).toLocaleTimeString()
 
 const logger = (_module) => {
-    return new(winston.Logger)({
-        transports: [
-            // colorize the output to the console
-            new(winston.transports.Console)({
-                timestamp: tsFormat,
-                colorize: true,
-                label: require.main.filename.slice(baseDir.length)
-            })
-        ]
-    });
-};
+  return new (winston.Logger)({
+    transports: [
+      // colorize the output to the console
+      new (winston.transports.Console)({
+        timestamp: tsFormat,
+        colorize: true,
+        label: _module.filename.slice(baseDir.length)
+      })
+    ]
+  })
+}
 
-module.exports = logger;
+module.exports = logger
