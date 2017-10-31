91bf4f564b69219599cd04ebb6c417aca4a07a63
Julien Couillard <julien.couillard@etudiant.univ-rennes1.fr>
2017-10-31 16:26:50 +0100

    update view trackSelection

--- routers/main.js
+++ routers/main.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models/controllers');
 
-const numberOfMostPlayed = 10;
+const numberOfTrend = 10;
 const numberOfUserMostPlayed = 10;
-
+const numberOfUserFavorite = 10;
 router
   .get("/", (req, res) => {
     res.render('index', {
@@ -21,30 +21,38 @@ router
 
 router
   .get('/trackSelection', function (req, res) {
-    // Trend track --> Youtube ? 
     // Most played tracks 
-    db.track.getMostPlayedTracks((err, mostPlayed) => {
+    db.track.getTrendTracks((err, trend) => {
       if (err) console.log(err);
-      var data;
-      data.mostPlayed = mostPlayed.slice(0, numberOfMostPlayed);
+      var data = {};
+      data.trend = trend.slice(0, numberOfTrend);
       // User Most Played Tracks 
-      // check UserID
-      db.track.getUserMostPlayedTracks(1, (err, userMostPlayed) => {
-        if (err) console.log(err);
-        data.userMostPlayed = userMostPlayed.slice(0, numberOfUserMostPlayed);
-        
-        // Favorite track --> player connected ? 
-        // db.user.getFavoriteTracks --> fav du user (avec l'objet entier)
-        res.render('trackSelection', data);
-      })
-      
+      if (req.user) {
+        db.track.getUserMostPlayedTracks(req.user.id, (err, userMostPlayed) => {
+            if (err) console.log(err);
+            data.userMostPlayed = userMostPlayed.slice(0, numberOfUserMostPlayed);
+            // Favorite User track
+            req.user.getFavoriteTracks((err, userFavorite) => {
+              if (err) console.log(err);
+              data.userFavorite = mostPlayed.slice(0, numberOfUserFavorite);
+              data.userConnected = true;
+              res.render('trackSelection', data);
+            })
+          })
+        }
+        else{
+          data.userConnected = false;
+          res.render('trackSelection', data);
+        }
     })
-    
+
   });
 
 router
   .get('/game/:id', function (req, res) {
-    res.render('game', {mostPlayed : req.params.id});
+    res.render('game', {
+      mostPlayed: req.params.id
+    });
   });
 
 
