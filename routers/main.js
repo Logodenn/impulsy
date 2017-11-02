ae25ff7edfad34df346e85088221f3d7995467c2
Julien Couillard <julien.couillard@etudiant.univ-rennes1.fr>
2017-11-02 18:49:35 +0100

    route /trackSelection/:id ok

--- routers/main.js
+++ routers/main.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const logger = require('./utils/artefacts')
+const artefacts = require('../utils/artefacts')
 const db = require('../models/controllers');
-var cookies = require( "cookies" )
-const youtube = require('./youtube');
+var cookies = require("cookies")
+const youtube = require('../modules/youtube');
 const numberOfTrend = 10;
 const numberOfUserMostPlayed = 10;
 const numberOfUserFavorite = 10;
 router
-.get("/", (req, res) => {
-    res.render('index', { message : "Hello world !"});
-});
+  .get("/", (req, res) => {
+    res.render('index', {
+      message: "Hello world !"
+    });
+  });
 
 router
   .get('/hallOfFame', function (req, res) {
@@ -29,21 +31,20 @@ router
       // User Most Played Tracks 
       if (req.user) {
         db.track.getUserMostPlayedTracks(req.user.id, (err, userMostPlayed) => {
+          if (err) console.log(err);
+          data.userMostPlayed = userMostPlayed.slice(0, numberOfUserMostPlayed);
+          // Favorite User track
+          req.user.getFavoriteTracks((err, userFavorite) => {
             if (err) console.log(err);
-            data.userMostPlayed = userMostPlayed.slice(0, numberOfUserMostPlayed);
-            // Favorite User track
-            req.user.getFavoriteTracks((err, userFavorite) => {
-              if (err) console.log(err);
-              data.userFavorite = userFavorite.slice(0, numberOfUserFavorite);
-              data.userConnected = true;
-              res.render('trackSelection', data);
-            })
+            data.userFavorite = userFavorite.slice(0, numberOfUserFavorite);
+            data.userConnected = true;
+            res.render('trackSelection', data);
           })
-        }
-        else{
-          data.userConnected = false;
-          res.render('trackSelection', data);
-        }
+        })
+      } else {
+        data.userConnected = false;
+        res.render('trackSelection', data);
+      }
     })
 
   });
@@ -51,52 +52,64 @@ router
 router
   .get('/trackSelection/:id', function (req, res) {
     var track;
-    if (Number.isInteger(req.params.id))
-    {
-      // c'est dans notre base
-      db.track.get(req.params.id, (err, result)=> {
-          if (err) console.log(err);
-          else track = result; 
-      })
-    }
-    else 
-    {
-      // c'est youtube  
+    var trackId = parseInt(req.params.id);    
+    if (Number.isInteger(trackId)) {
+      console.log("number");
+      // It's from our database
+      db.track.get(trackId, (err, result) => {
+        if (err) console.log(err);
+        else track = result;
+        console.log(track);
+      });
+    } else {
+      console.log("youtube");
+      // It's from youtube  
       // Savoir si la musique existe 
-      // ajouter à la bdd 
-      // méthode de Pierre pour récupérer 
-      // ici local à false
-      youtbue.getInfo(req.params.id, (err, reuslt) =>{
+      db.track.getTrackLink(req.params.id, (err,result) => {
         if (err) console.log(err);
         else {
-          soundName = reuslt.title;
-          youtube.getAudioStream(soundName, local, "lowest", function (err, stream) {
-            if (err) logger.error(err);
-            else {
-              youtube.getBars(stream, 1, function (err, bars) {
-                if (err) logger.error(err);
-                else {
-                  var arraySpectrum = bars;
-                  // On fou ou le getArrayArthefacts ?
-                  var arrayArtefacts = getArrayArthefacts(arraySpectrum); // array of 0, 1, 2, 3 --- 0 upper and 3 lowest 
-                  track_information = {
-                    arraySpectrum: arraySpectrum,
-                    arrayArtefacts: arrayArtefacts
-                  };
-                  track = {
-                    name: soundName,
-                    link: req.params.id,
-                    information: track_information
-                  };
-                  db.track.create(track, function (err, result) {
-                    if (err) logger.error(err);
-                    //else game.trackId = result.trackId;
-                  });
-                  logger.debug('Track saved !')
-                }
-              });
-            }
-          });
+          if (result) {
+            track = result;
+            console.log("track en db");
+          } else {
+            local = false;
+            youtube.getInfo(req.params.id, (err, result) => {
+              if (err) console.log(err);
+              else {
+                console.log(result);
+                soundName = result.title;
+                youtube.getAudioStream(soundName, local, "lowest", function (err, stream) {
+                  if (err) console.log(err);
+                  else {
+                    youtube.getBars(stream, 1, function (err, bars) {
+                      if (err) console.log(err);
+                      else {
+                        var arraySpectrum = bars;
+                        var arrayArtefacts = artefacts.getArrayArthefacts(arraySpectrum); // array of 0, 1, 2, 3 --- 0 upper and 3 lowest 
+                        track_information = {
+                          arraySpectrum: arraySpectrum,
+                          arrayArtefacts: arrayArtefacts
+                        };
+                        var track = {
+                          name: soundName,
+                          link: req.params.id,
+                          information: track_information
+                        };
+                        // add track to database 
+                        db.track.create(track, function (err, result) {
+                          if (err) console.log(err);
+                          else track.id = result.id;
+                        });
+                        //dPdhFOH1rxM
+                        console.log("track youtube inconnue");
+                        console.log(track);
+                      }
+                    });
+                  }
+                });
+              }
+            });
+          }
         }
       });
     }
