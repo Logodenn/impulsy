fdc0602758ed8bedfd0fc96b3b1fe1ec1e311f52
Benjent <benjent@hotmail.fr>
2017-11-09 18:28:09 +0100

    Handle dirty disabled input

--- assets/js/trackSelection.js
+++ assets/js/trackSelection.js
@@ -1,6 +1,6 @@
 
 function selectTrack(element) {
+    // document.querySelector("#selectedTrack").innerHTML = element.innerHTML;
+    document.querySelector("#selectedTrack").value = element.innerHTML;
     // console.log(element.innerHTML);
-    // selectedTrack = element.innerHTML;
-    document.querySelector("#selectedTrack").innerHTML = element.innerHTML;
 }
