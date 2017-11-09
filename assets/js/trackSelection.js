627d7f4a7b9e00ae0f141115d3b3d2c4611dfe1a
Benjent M <Benjent@hotmail.fr>
2017-11-09 17:38:56 +0100

    Display the selected track in the difficulty modal

---
+++ assets/js/trackSelection.js
@@ -0,0 +1,6 @@
+
+function selectTrack(element) {
+    // console.log(element.innerHTML);
+    // selectedTrack = element.innerHTML;
+    document.querySelector("#selectedTrack").innerHTML = element.innerHTML;
+}
