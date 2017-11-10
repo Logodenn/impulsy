9566489035064b876b4f46cbee69d88e0679edc2
Pierre Charpentier <charpentier.pierre@outlook.com>
2017-11-10 13:51:18 +0100

    Create audio module

---
+++ modules/audio.js
@@ -0,0 +1,15 @@
+const ffmpeg = require('fluent-ffmpeg')
+const ytdl = require('ytdl-core')
+
+const getStream = (source, callback) => {
+    
+}
+
+module.exports = {
+    getLocalStream: (options, callback) => {
+
+    },
+    getYoutubeStream: (options, callback) => {
+
+    }
+}
