1a7b66101edcc37fed6bb641843694dc666e8a00
Benjent M <benjaminjeanmorvan@gmail.com>
2017-11-11 16:46:39 +0100

    Add the route

--- routers/howItWorks.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get("/howItWorks", (req, res) => {
-    res.render('howItWorks', {message: "hello world"});
-});
-
-module.exports = router;
