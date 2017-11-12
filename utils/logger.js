4a54c89518cbd2c2ce25b46f62db12971f1fdc4b
Qayou <charpentier.pierre@outlook.com>
2017-11-12 13:46:31 +0100

    Add levels to logger

--- utils/logger.js
+++ utils/logger.js
@@ -5,15 +5,23 @@ const tsFormat = () => (new Date()).toLocaleTimeString()
 
 const logger = (_module) => {
   return new (winston.Logger)({
+    levels: {
+      info: 0,
+      debug: 1,
+      error: 2
+    },
     transports: [
       // colorize the output to the console
       new (winston.transports.Console)({
         timestamp: tsFormat,
         colorize: true,
-        label: _module.filename.slice(baseDir.length)
+        label: _module.filename.slice(baseDir.length),
+        level: 'error'
       })
     ]
   })
 }
+
+logger.level = 'debug'
 
 module.exports = logger
