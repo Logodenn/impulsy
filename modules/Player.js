457a45669cd14cd39f1b3d6ea665c8889348628e
Qayou <charpentier.pierre@outlook.com>
2017-11-12 13:42:49 +0100

    Work on back refactoring

--- modules/Player.js
+++ modules/Player.js
@@ -1,22 +1,18 @@
-'use strict'
+const logger = require('../utils/logger')(module)
 
 module.exports = class Player {
   constructor (socket) {
     this.socket = socket
+    this.id = socket.id
+
+    this._position = 0
+    this.name = 'Guest 1'
   }
 
-  bindEvents () {
-    // Player Events
-    this.socket.on('playerMove', this.onPlayerMove)
-    this.socket.on('endGame', this.onEndGame)
-    this.socket.on('disconnect', this.onDisconnect)
-  }
-
-  onPlayerMove () {
-
-  }
-
-  onEndGame () {
-
+  set position (position) {
+    // Check if this position is possible
+    if ([0, 1, 2, 3].includes(position)) {
+      this._position = position
+    }
   }
 }
