716af6cc42f3b07d58a5c3d30e848366734ef02a
Julien Couillard <julien.couillard@etudiant.univ-rennes1.fr>
2017-11-13 22:55:51 +0100

    Spectrum Object with is creation

--- modules/Spectrum.js
+++ modules/Spectrum.js
@@ -1,36 +1,43 @@
 const youtube = require('./youtube');
 
+/**
+	 * Object Spectrum is the envelop of the sound
+	 * @attribute {string} name name of the sound 
+	 * @attribute {string} link link of the sound if is from Youtube 
+	 * @attribute {array} bars array of Bar object 
+	 * @attribute {int} barsPerSeconds int number of bar per second (speed of the song)
+	*/
 module.exports = class Spectrum {
 	constructor() {
 		this.name = null // name of the sound 
-		this.bars = []
+		this.link = null
+		this.bars = [] // This is track information
 		this.barsPerSeconds = 2 // Number of bars per seconds for youtube modules
 	}
 
-	// Sound : Youtube Id or title 
-	// local : if youtube False else True 
+	/**
+	 * Function createSpectrum create the envelop of the sound
+	 * @attribute {string} sound link or name of the sound
+	 * @attribute {bool} local False if is from Youtube, True if is from local storage
+	*/
 	createSpectrum(sound, local) {
 		youtube.getAudioStream(sound, local, "lowest", function (err, stream) {
 			if (err) console.log(err);
 			else {
-				youtube.getBars(stream, barsPerSeconds, function (err, bars) {
+				youtube.getBars(stream, barsPerSeconds, function (err, barsAmplitude) {
 					if (err) console.log(err);
 					else {
-						// TODO : call bar method
-						/*
-						var arraySpectrum = bars;
-						var arrayArtefacts = getArrayArthefacts(arraySpectrum); // array of 0, 1, 2, 3 --- 0 upper and 3 lowest 
-						track_information = {
-							arraySpectrum: arraySpectrum,
-							arrayArtefacts: arrayArtefacts
-						};
-						var track = {
-							name: result.title,
-							link: req.params.id,
-							information: track_information
-						};
-						*/
+						barsAmplitude.forEach(function(barAmplitude) {
+							bar = new Bar();
+							bar.create(barAmplitude);
+							this.bars.append(bar)
+						  });
 						// add track to database 
+						var track = {
+							name: this.name,
+							link: this.link,
+							information: this.bars
+						};		
 						db.track.create(track, function (err, result) {
 							if (err) console.log(err);
 						});
@@ -38,10 +45,6 @@ module.exports = class Spectrum {
 				});
 			}
 		});
-	}
-
-	loadBars(idSound) {
-
 	}
 }
 
