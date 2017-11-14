7a28998d32c9ca32847a00968f2b87decf8c3989
Julien Couillard <Texju@iMac-de-Julien.local>
2017-11-14 13:51:12 +0100

    create artefacts for the two players

--- modules/Spectrum.js
+++ modules/Spectrum.js
@@ -4,10 +4,11 @@ const FREQUENCY_CHECKING = 10;
 /**
  * Const for the artefact 
  */
-const NUMBER_OF_POSITION = 4;
-const AMPLITUDE_MAX = 1
-const BASE_LOWER_BOUND = 1
-const BASE_UPPPER_BOUND = 2
+const NUMBER_OF_POSITIONS = 4;
+const AMPLITUDE_MAX = 1;
+const BASE_LOWER_BOUND = 1;
+const BASE_UPPPER_BOUND = 2;
+const MINIMUM_AMPLITUDE = 0.05;
 
 /**
  * Object Spectrum is the envelop of the sound
@@ -81,8 +82,7 @@ module.exports = class Bar {
 	constructor() {
 		this.id = null // number of the bar in this spectrum 
 		this.amplitude = null
-		this.slots = []
-		this.artefacts = []
+		this.artefacts = [] // artefact position 
 		this.positionPlayers = []
 		this.isCurrentBar = false;
 		//this.rand = null
@@ -137,32 +137,50 @@ module.exports = class Bar {
 	 * @param {float} barAmplitude amplitude of the bar
 	 * @param {int} number position of the bar in this spectrum
 	 */
-	// TODO : remake for one amplitude and one random Number
 	create(barAmplitude, number) {
 		console.log("Generation of the bar"+number+" with amplitude = "+barAmplitude);
 		this.id = number;
 		this.amplitude = barAmplitude;
 		var lowerBound = BASE_LOWER_BOUND;
 		var upperBound = BASE_UPPPER_BOUND;
-		var limit = AMPLITUDE_MAX / (NUMBER_OF_POSITION/2);
+		var limit = AMPLITUDE_MAX / (NUMBER_OF_POSITIONS/2);
 		/*
 			Settings for bound
-			The smallest bar is bet
+			Check if the amplitude is bigger than the limit 
+			if it's bigger the bar will have more postion 
 		*/
-		do {
-			if(this.amplitude>limit){
-				limit+=limit;
-				lowerBound--;
-				upperBound++;
-			}
-			else{
-				break;
-			}
+		/*
+		if (this.amplitude < MINIMUM_AMPLITUDE)
+		{
+		*/
+			do {
+				if(this.amplitude>limit){
+					limit+=limit;
+					lowerBound--;
+					upperBound++;
+				}
+				else{
+					break;
+				}
+			} while (limit <= AMPLITUDE_MAX);
+
+			// Add first artefact for player 1
+			this.artefacts.append(Math.round(Math.random() * (upperBound - lowerBound) + lowerBound));
+			// Check if the two player haven't got the same artefact position
+			var artefactPlayer2 = 0
+			do{
+				artefactPlayer2 = Math.round(Math.random() * (upperBound - lowerBound) + lowerBound)
+				
+			}while (this.artefacts[0] == artefactPlayer2);
+			// Add second artefact for player 2
+			this.artefacts.append(artefactPlayer2);
+		/*
 		}
-		while (limit <= AMPLITUDE_MAX);
-		// Add first artefact 
-		this.artefacts.append(Math.round(Math.random() * (upperBound - lowerBound) + lowerBound));
-		// TODO : ajout artefact pour second joueur
+		else {
+			this.artefacts.append(null);
+			this.artefacts.append(null);
+		}
+		*/
 	}
 
 }
