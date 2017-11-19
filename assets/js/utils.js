9cb38edad2b2cfdffd1dae2cd2bc2b48214cc46e
Benjent M <benjaminjeanmorvan@gmail.com>
2017-11-19 23:39:36 +0100

    Move mail validation function to utils

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -12,3 +12,11 @@ function toggleElement(element, action) {
         element.attributes.state.value = "closed";
     }
 }
+
+function validateMail() {
+    
+    var mail = document.querySelector("#mail").value;
+    var regex = /^(([^<>()[\]\\.,;:\s@\“]+(\.[^<>()[\]\\.,;:\s@\“]+)*)|(\“.+\“))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    return regex.test(mail);
+}
