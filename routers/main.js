5855df2a7c68e77a0b3b7ae1b6aeacb08526977d
Benjent M <Benjent@hotmail.fr>
2017-11-21 18:08:06 +0100

    Remove old trackSelection router

--- routers/main.js
+++ routers/main.js
@@ -76,32 +76,6 @@ router.get('/howItWorks', function (req, res) {
   res.render('howItWorks', data);
 })
 
-router.get('/trackSelection', function (req, res) {
-  // Most played tracks
-  db.track.getTrendTracks((err, trend) => {
-    if (err) console.log(err)
-    var data = {}
-    data.trend = trend.slice(0, numberOfTrend)
-    // User Most Played Tracks 
-    if (req.user) {
-      db.track.getUserMostPlayedTracks(req.user.id, (err, userMostPlayed) => {
-        if (err) console.log(err)
-        data.userMostPlayed = userMostPlayed.slice(0, numberOfUserMostPlayed)
-        // Favorite User track
-        req.user.getFavoriteTracks((err, userFavorite) => {
-          if (err) console.log(err)
-          data.userFavorite = userFavorite.slice(0, numberOfUserFavorite)
-          data.userConnected = true
-          res.render('trackSelection', data)
-        })
-      })
-    } else {
-      data.userConnected = false
-      res.render('trackSelection', data)
-    }
-  })
-})
-
 router.get('/difficulty/:id', function (req, res) {
   var track;
   var trackId = parseInt(req.params.id);
