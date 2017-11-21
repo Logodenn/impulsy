bb43d697759a1cd0fcda2d35837a4ecd9172363d
Qayou <charpentier.pierre@outlook.com>
2017-11-21 12:15:49 +0100

    Add moa track when creating db

--- modules/Spectrum.js
+++ modules/Spectrum.js
@@ -5,12 +5,11 @@ const Bar = require('./Bar')
 const FREQUENCY_CHECKING = 10
 const BAR_PER_SECONDS = 2
 
-
 /**
  * Object Spectrum is the envelop of the sound
- * @attribute {string} name name of the sound 
- * @attribute {string} link link of the sound if is from Youtube 
- * @attribute {array} bars array of Bar object 
+ * @attribute {string} name name of the sound
+ * @attribute {string} link link of the sound if is from Youtube
+ * @attribute {array} bars array of Bar object
  * @attribute {int} barsPerSeconds int number of bar per second (speed of the song)
  */
 module.exports = class Spectrum {
@@ -18,7 +17,7 @@ module.exports = class Spectrum {
     this.name = null // name of the sound
     this.link = null
     this.bars = [] // This is track information
-    //this.barsPerSeconds = 2 // Number of bars per seconds for youtube modules
+    // this.barsPerSeconds = 2 // Number of bars per seconds for youtube modules
   }
 
   /**
@@ -27,49 +26,47 @@ module.exports = class Spectrum {
    * @attribute {bool} local False if is from Youtube, True if is from local storage
    */
   createSpectrum (sound, local, cb) {
-    let getStream = audio.getYoutubeStream;
-    let self = this;
+    let getStream = audio.getYoutubeStream
+    let self = this
 
     if (local) {
       getStream = audio.getLocalStream
     }
-    audio.getInfo(sound, (err, res) =>{
-      self.link = res.id;
-      self.name = res.title;
+    audio.getInfo(sound, (err, res) => {
+      self.link = res.id
+      self.name = res.title
+
       getStream({
         videoId: sound,
         fileName: sound,
         quality: 'lowest'
       }, function (err, stream) {
         if (err) {
-          console.log(err);
+          console.log(err)
           return cb(err)
-        }
-        else {
+        } else {
           audio.getAmplitudes(stream, BAR_PER_SECONDS, function (err, barsAmplitude) {
             if (err) {
-              console.log(err);
+              console.log(err)
               return cb(err)
-            }
-            else {
+            } else {
               barsAmplitude.forEach(function (barAmplitude, i) {
-                let bar = new Bar();
-                bar.create(barAmplitude, i);
-                self.bars.push(bar);
-              });
-  
+                let bar = new Bar()
+                bar.create(barAmplitude, i)
+                self.bars.push(bar)
+              })
+
               // add track to database
               const track = {
                 name: self.name,
                 link: self.link,
                 information: self.bars
-              };
+              }
               db.track.create(track, function (err, result) {
                 if (err) {
-                  console.log(err);
+                  console.log(err)
                   return cb(err)
-                }
-                else {
+                } else {
                   self.id = result.id
                   cb(null, self)
                 }
@@ -79,7 +76,6 @@ module.exports = class Spectrum {
         }
       })
     })
-
   }
 
   /**
@@ -90,17 +86,15 @@ module.exports = class Spectrum {
     db.track.get(id, (err, result) => {
       if (err) console.log(err)
       else {
-        this.name = result.name;
-        this.link = result.link;
-        this.bars = result.information;
+        this.name = result.name
+        this.link = result.link
+        this.bars = result.information
         cb(null, this)
       }
     })
   }
-  
-  checkArtefacts(barNumber, player)
-	{
-		return this.bars[barNumber].checkArtefact(player)
+
+  checkArtefacts (barNumber, player)	{
+    return this.bars[barNumber].checkArtefact(player)
   }
-  
 }
