95c44722ef9a2c9e0d374fca606050d5f0c828ec
Qayou <charpentier.pierre@outlook.com>
2018-01-19 14:33:12 +0100

    Add number back to the player class

--- modules/Player.js
+++ modules/Player.js
@@ -1,13 +1,13 @@
 const logger = require('../utils/logger')(module)
 
 module.exports = class Player {
-  constructor (socket, position, user) {
-    this.socket = socket
-    this.number = number
+  constructor (socket, number, position, user) {
     this.id = socket.id
-    this.takenArtefactsCount = 0
-    this.position = position
+    this.number = number
+    this.socket = socket
     this.artefactsTaken = []
+    this.position = position
+    this.takenArtefactsCount = 0
 
     if (user.logged_in) {
       this.user = {}
@@ -31,8 +31,9 @@ module.exports = class Player {
     } else {
       this._position.y = this._position.y || 1
     }
-   // TODO : check x
-   this._position.x=position.x
+
+    // TODO : check x
+    this._position.x = position.x
   }
 
   get position () {
