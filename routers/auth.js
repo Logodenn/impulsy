1770d26a8b4ad199a77f4e2b1f95016aa1feca1b
berrou <glennberr29@gmail.com>
2018-01-27 13:33:58 +0100

    signup

--- routers/auth.js
+++ routers/auth.js
@@ -13,13 +13,17 @@ router.get('/login/:problem?', function(req, res) {
     res.render('login',data);
 });
 
-router.get('/signup', function(req, res) {
-  res.render('signup');
+router.get('/signup/:problem?', function(req, res) {
+  var data = {}
+  if (req.params.problem){
+    data.problem=req.params.problem
+  }
+  res.render('signup',data);
 });
 
 router.post('/signup', passport.authenticate('local-signup', {
       successRedirect : '/', // redirect to the secure profile section
-      failureRedirect : '/signup', // redirect back to the signup page if there is an error
+      failureRedirect : '/signup/failure', // redirect back to the signup page if there is an error
       failureFlash : false // allow flash messages
     }));
 
